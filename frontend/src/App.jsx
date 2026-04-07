7e68be13e63fdcdd59ae09e92e78fc39f2d081f9
Kiri Lefebvre <klefebvr@myune.edu.au>
2026-04-07 23:36:44 +1000

    Set up pages structure and basic routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import { Home, Analysis, Results } from './pages'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <Header/>
-    <main className="main"></main>
+      <Header/>
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="analysis" element={<Analysis />} />
+          <Route path="results" element={<Results />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </main>
     </>
   )
 }
