b2f0974eb1875ff06198a3c388098c16bf377b47
Kiri Lefebvre <klefebvr@myune.edu.au>
2026-04-08 23:41:06 +1000

    Stub API client call to Get Models

--- frontend/src/pages/Analysis.jsx
+++ frontend/src/pages/Analysis.jsx
@@ -1,5 +1,24 @@
+import { useState, useEffect } from 'react'
+
 export default function Analysis() {
+    const [models, setModels] = useState([])
+    
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/api/models")
+              .then((response) => response.json())
+              .then((json) => setModels(json.models))
+              .catch((error) => console.error('Error fetching data: ', error))
+    }, [])
+
     return (
-        <div>Analysis</div>
+        <>
+          <div>Analysis</div>
+          <ul>
+            {models.map((model, index) => (
+                <li key={index}>{model}</li>
+            ))}
+          </ul>
+        </>
+
     )
 }
