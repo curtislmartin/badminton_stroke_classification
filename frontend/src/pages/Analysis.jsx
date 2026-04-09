a5f3dca9b7805bc02b0b9ccb7d34c45370cbed27
Kiri Lefebvre <klefebvr@myune.edu.au>
2026-04-09 21:15:10 +1000

    Stub API call to Get Status

--- frontend/src/pages/Analysis.jsx
+++ frontend/src/pages/Analysis.jsx
@@ -2,7 +2,9 @@ import { useState, useEffect } from 'react'
 
 export default function Analysis() {
     const [models, setModels] = useState([])
+    const [status, setStatus] = useState('')
     
+    // Run once on page load, to get available models
     useEffect(() => {
         fetch("http://127.0.0.1:8000/api/models")
               .then((response) => response.json())
@@ -10,14 +12,32 @@ export default function Analysis() {
               .catch((error) => console.error('Error fetching data: ', error))
     }, [])
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetch("http://127.0.0.1:8000/api/status/123")
+              .then((response) => response.json())
+              .then((json) => {
+                setStatus(json.status)
+                console.log(json.status)
+                if (json.status === "complete") {
+                    clearInterval(interval) // stop polling
+                }
+              })
+              .catch((error) => console.error('Error fetching data: ', error))
+        }, 2000)
+        return () => clearInterval(interval)
+    }, [])
+
     return (
         <>
           <div>Analysis</div>
+          <div>Select a model:</div>
           <ul>
             {models.map((model, index) => (
                 <li key={index}>{model}</li>
             ))}
           </ul>
+          <div>Stroke classification is: {status}</div>
         </>
 
     )
